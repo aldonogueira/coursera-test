c5d362bf0c853d49df7c8c8a2e693d76477585f4
Aldo Nogueira <aldo@tecgraf.puc-rio.br>
2017-05-10 12:24:52 -0300

    Adicionando funcionalidade

--- semana7/js/app.js
+++ semana7/js/app.js
@@ -1,11 +1,26 @@
-(function() {
+(function () {
   'use strict';
-  
+
   angular.module('LunchCheck', [])
-  .controller('LunchCheckController', LunchCheckController);
+    .controller('LunchCheckController', LunchCheckController);
 
   LunchCheckController.$inject = ['$scope'];
   function LunchCheckController($scope) {
+    $scope.lunchMenu = '';
+    $scope.message = '';
 
+    $scope.checkLunch = function () {
+      if ($scope.lunchMenu == '') {
+        $scope.message = 'Please enter data first';
+        return;
+      }
+
+      var count = $scope.lunchMenu.split(',').length;
+      if (count <= 3) {
+        $scope.message = 'Enjoy!';
+      } else {
+        $scope.message = 'Too much!';
+      }
+    };
   }
 })();
