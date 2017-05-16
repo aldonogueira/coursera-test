468d6704f036a5d7170d645f2793a0c0838878c0
Aldo Nogueira <aldo@tecgraf.puc-rio.br>
2017-05-16 12:41:59 -0300

    Implementa o serviço

--- semana8/js/app.js
+++ semana8/js/app.js
@@ -17,6 +17,35 @@
   }
 
   function ShoppingListCheckOffService() {
+    var service = this;
 
+    var toBuyList = [
+      {name: "Cookie A", quantity: 10},
+      {name: "Cookie B", quantity: 11},
+      {name: "Cookie C", quantity: 12},
+      {name: "Cookie D", quantity: 13},
+      {name: "Cookie E", quantity: 14}
+    ];
+    var boughtList = [];
+
+    service.buy = function(index) {
+      var element = toBuyList[index];
+      toBuyList.splice(index, 1);
+      boughtList.push(element);
+    };
+
+    service.giveBack = function(index) {
+      var element = boughtList[index];
+      boughtList.splice(index, 1);
+      toBuyList.push(element);
+    };
+
+    service.getToBuyList = function() {
+      return toBuyList;
+    };
+
+    service.getBoughtList = function() {
+      return boughtList;
+    };
   }
 })();
