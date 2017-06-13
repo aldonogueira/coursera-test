d702380af7e939065e15cd380464be8a49c505be
Aldo Nogueira <aldo@tecgraf.puc-rio.br>
2017-06-13 16:36:19 -0300

    Cria o serviço

--- semana9/js/app.js
+++ semana9/js/app.js
@@ -1,10 +1,36 @@
 (function () {
   'use strict';
 
-  angular.module('NarrowItDownApp'[])
-  .controller('NarrowItDownController', NarrowItDownController);
+  angular.module('NarrowItDownApp', [])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService);
 
-  function NarrowItDownController() {
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+    var listPromise = MenuSearchService.getMatchedMenuItems('Chicken');
+    listPromise.then(function(e) {
+      console.log(e);
+    });
+  }
+
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http) {
+    var service = this;
+
+    service.getMatchedMenuItems = function(searchTerm) {
+      return $http({
+        method: 'GET',
+        url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+      }).then(function(response) {
+        var items = response.data.menu_items;
+        return items.filter(function(e) {
+           return e.description.toLowerCase()
+             .search(searchTerm.toLowerCase()) != -1;
+         });
+      }).catch(function(error) {
+        console.log('Erro ' + error.message);
+      });
+    }
 
   }
 
