562940a410ce3700d2f967634d4dfe55de1f1697
Aldo Nogueira <aldo@tecgraf.puc-rio.br>
2017-06-19 16:06:12 -0300

    Componente categories

---
+++ semana10/src/menuapp/categories.component.js
@@ -0,0 +1,22 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.component('categories', {
+  templateUrl: 'menuapp/templates/categories.template.html',
+  controller: CategoriesComponentController
+});
+
+CategoriesComponentController.$inject = ['MenuDataService'];
+function CategoriesComponentController(MenuDataService) {
+  var $ctrl = this;
+
+  $ctrl.categoriesList = [];
+
+  MenuDataService.getAllCategories()
+  .then(function(data) {
+    $ctrl.categoriesList = data;
+  });
+}
+
+})();
