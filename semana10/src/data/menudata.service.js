4cebcae52f901a0133394f3f07b1242f8897ad72
Aldo Nogueira <aldo@tecgraf.puc-rio.br>
2017-06-19 14:35:52 -0300

    Criação do serviço menudata

---
+++ semana10/src/data/menudata.service.js
@@ -0,0 +1,20 @@
+(function () {
+'use strict';
+
+angular.module('data')
+.service('MenuDataService', MenuDataService);
+
+function MenuDataService() {
+  var service = this;
+
+  service.getAllCategories = function() {
+
+  };
+
+  service.getItemsForCategory = function(categoryShortName) {
+
+  };
+
+}
+
+})();
