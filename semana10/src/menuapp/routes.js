e06ad4a3d01ab79658ad26abd5753c0c2bb0c27e
Aldo Nogueira <aldo@tecgraf.puc-rio.br>
2017-06-20 11:11:15 -0300

    Adicionando as rotas

---
+++ semana10/src/menuapp/routes.js
@@ -0,0 +1,36 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/menuapp/templates/home.template.html'
+  })
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/menuapp/templates/main-categories.template.html',
+    resolve: {
+      categories: ['MenuDataService', function(MenuDataService){
+        return MenuDataService.getAllCategories();
+      }]
+    }
+  })
+  .state('items', {
+    url: '/items/{shortName}',
+    templateUrl: 'src/menuapp/templates/main-items.template.html',
+    resolve: {
+      items: ['MenuDataService', function(MenuDataService){
+        return MenuDataService.getItemsForCategory(shortName);
+      }]
+    }
+  });
+}
+
+})();
