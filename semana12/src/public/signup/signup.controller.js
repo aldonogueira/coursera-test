98be78cb08bcc2bdbb9ae88b3038f9cf95eed3cd
Aldo Nogueira <aldo@tecgraf.puc-rio.br>
2017-06-29 16:02:29 -0300

    Conferindo existência do prato

--- semana12/src/public/signup/signup.controller.js
+++ semana12/src/public/signup/signup.controller.js
@@ -4,8 +4,8 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = [];
-function SignUpController() {
+SignUpController.$inject = ['$http'];
+function SignUpController($http) {
   var signUpCtrl = this;
 
   signUpCtrl.firstname = null;
@@ -13,9 +13,19 @@ function SignUpController() {
   signUpCtrl.email = null;
   signUpCtrl.phone = null;
   signUpCtrl.favoritedish = null;
+  signUpCtrl.submitted = false;
+  signUpCtrl.error = false;
 
   signUpCtrl.submit = function() {
-    console.log('firstname = ' + signUpCtrl.firstname)
+    var url = 'https://aldo-coursera.herokuapp.com/menu_items/' + signUpCtrl.favoritedish + '.json';
+    $http.get(url)
+      .then(function(result) {
+        signUpCtrl.submitted = true;
+        signUpCtrl.error = false;
+      }).catch(function(){
+        signUpCtrl.submitted = false;
+        signUpCtrl.error = true;
+      });
   };
 }
 
