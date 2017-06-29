d7db899679c2de92f2c297e8f33a4e98c6598f0e
Aldo Nogueira <aldo@tecgraf.puc-rio.br>
2017-06-29 15:44:33 -0300

    Botão submit

--- semana12/src/public/signup/signup.controller.js
+++ semana12/src/public/signup/signup.controller.js
@@ -8,6 +8,15 @@ SignUpController.$inject = [];
 function SignUpController() {
   var signUpCtrl = this;
 
+  signUpCtrl.firstname = null;
+  signUpCtrl.lastname = null;
+  signUpCtrl.email = null;
+  signUpCtrl.phone = null;
+  signUpCtrl.favoritedish = null;
+
+  signUpCtrl.submit = function() {
+    console.log('firstname = ' + signUpCtrl.firstname)
+  };
 }
 
 })();
